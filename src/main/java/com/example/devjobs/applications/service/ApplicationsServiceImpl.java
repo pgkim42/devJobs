14b5b035331082e3a0b60c10c7066555459e4244
ShimT <sth2721@gmail.com>
2024-12-09 01:21:03 +0900

    1209 applications

--- src/main/java/com/example/devjobs/applications/service/ApplicationsServiceImpl.java
+++ src/main/java/com/example/devjobs/applications/service/ApplicationsServiceImpl.java
@@ -1,7 +1,13 @@
 package com.example.devjobs.applications.service;
 
+import com.example.devjobs.applications.repository.ApplicationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ApplicationsServiceImpl implements ApplicationsService{
 
-
+    @Autowired
+    ApplicationsRepository repository;
 
 }
