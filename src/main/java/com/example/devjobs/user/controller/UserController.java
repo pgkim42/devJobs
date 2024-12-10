4f9852a94ef8a25c6c477e6ed551c07bd5eabb1e
seongmin <sungmin990923@naver.com>
2024-12-10 22:04:49 +0900

    1210 마이페이지 내 비밀번호수정 클릭 시 비밀번호 검증

--- src/main/java/com/example/devjobs/user/controller/UserController.java
+++ src/main/java/com/example/devjobs/user/controller/UserController.java
@@ -1,33 +1,38 @@
 package com.example.devjobs.user.controller;
 
+import com.example.devjobs.user.dto.request.auth.PasswordRequest;
 import com.example.devjobs.user.dto.request.auth.UpdateUserRequestDto;
+import com.example.devjobs.user.entity.User;
 import com.example.devjobs.user.service.UserService;
 import com.example.devjobs.user.service.implement.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    @PutMapping("/myPage")
-    public ResponseEntity<String> updateUserInfo(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody UpdateUserRequestDto dto
-    ) {
-        userService.updateUserInfo(userDetails.getUsername(), dto);
-        return ResponseEntity.ok("회원 정보가 수정되었습니다.");
+    @PostMapping("/verify-password")
+    public ResponseEntity<?> verifyPassword(@RequestBody PasswordRequest request, Principal principal) {
+        String currentUserId = principal.getName();
+        User user = userService.findUserById(currentUserId);
+
+        if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ResponseEntity.ok(Map.of("success", true));
+        } else {
+            return ResponseEntity.ok(Map.of("success", false));
+        }
     }
 
-    @DeleteMapping("/myPage")
-    public ResponseEntity<String> deleteUser(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        userService.deleteUser(userDetails.getUsername());
-        return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
-    }
 }
 
