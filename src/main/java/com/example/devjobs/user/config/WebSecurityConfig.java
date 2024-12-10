7a550e8461ff4c01d325635a40488e9d471d1e73
seongmin <sungmin990923@naver.com>
2024-12-10 12:19:06 +0900

    security properties 수정

--- src/main/java/com/example/devjobs/user/config/WebSecurityConfig.java
+++ src/main/java/com/example/devjobs/user/config/WebSecurityConfig.java
@@ -57,9 +57,9 @@ public class WebSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**").permitAll()
-                        .requestMatchers("/api/v1/company/**").hasRole("COMPANY")
-                        .requestMatchers("/api/v1/user/**").hasRole("USER")
+                        .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**", "/jobcategory/**").permitAll()
+                        .requestMatchers("/api/v1/company/**", "/companyprofile/**", "/jobposting/**").hasRole("COMPANY")
+                        .requestMatchers("/api/v1/user/**", "/simlilarposting/**", "/resume/**", "/apply/**").hasRole("USER")
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
